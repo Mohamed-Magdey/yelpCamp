d84da82b4cf99b4abb556de9467919ccc15d24f8
mohamed <ragabmohame6@gmail.com>
2020-04-12 17:10:20 +0200

    typo error

--- models/comment.js
+++ models/comment.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-var mongoose = require("mongoose");
-
-var commentSchema = mongoose.Schema({
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
-    },
-    text: String
-});
-
-module.exports = mongoose.model("Comment", commentSchema);
-||||||| merged common ancestors
-=======
 const mongoose = require('mongoose');
 
 let commentSchema = new mongoose.Schema({
@@ -29,4 +12,3 @@ let commentSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('Comment', commentSchema);
->>>>>>> b28c6304bb2ab56b8827ce28146d93842e8d6691
